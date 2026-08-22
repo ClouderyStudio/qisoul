ffabe168f5af9dda459181e12f5a88de249a1468
TulipQiyi <justqiyi@outlook.com>
2026-08-22 14:37:08 +0800

    refactor(main): 移除未使用的 casdoor-vue-sdk 注册

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,12 @@ import { createApp } from 'vue'
 import './styles/main.css'
 import App from './App.vue'
 import { router } from './router/router.ts'
-import Casdoor from 'casdoor-vue-sdk'
 import { createPinia } from 'pinia'
 import { useTheme } from './composables/useTheme'
-
-const casdoorConfig = {
-    serverUrl: 'https://auth.cldery.com',
-    clientId: '092662583ec82c3f3a96',
-    appName: 'app-built-in',
-    organizationName: 'built-in',
-}
 
 const app = createApp(App)
 
 app.use(createPinia())
-
-app.use(Casdoor, casdoorConfig)
 
 app.use(router)
 
